7f91f0fd36ca02a3a4d77297a7de080bba24f1df
ouvreboite <muscatjb@gmail.com>
2022-08-17 09:59:20 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# flutter-map-benchmark
+Testing out various mapping libraries in Flutter
---
+++ android/app/src/main/kotlin/com/example/flutter_map_benchmark/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_map_benchmark
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
